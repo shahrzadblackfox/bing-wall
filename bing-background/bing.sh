33786cd8a1a016be73bbed4c95275abecf78ad99
shahrzadblackfox <75183431+shahrzadblackfox@users.noreply.github.com>
2020-12-05 21:56:10 +0330

    Update bing.sh

--- bing-background/bing.sh
+++ bing-background/bing.sh
@@ -4,21 +4,25 @@ bing="http://www.bing.com"
 
 xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-WW"
 
-savDir=$HOME'/Pictures/'
+saveDir=$HOME'/Pictures/'
 picOpts="zoom"
-picRes="_1200x680"
+#made mistake here, always check for standard values.
+picRes="_1280x720"
 picExt=".jpg"
+#be careful of the spaces. 
 name=$(date +"%Y-%m-%d")
 picURL=$bing$(echo $(curl -s $xmlURL) | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt
 tempName=$name".jpg"
+
 
 curl -s -o $saveDir$tempName $picURL
 
 
 
-DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-uri file://$deskDir$picFileName
+DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-uri file://$saveDir$tempName
 
 DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-options $picOpts
+ln -s /Projects/ /bin/ 
 
 
 exit
